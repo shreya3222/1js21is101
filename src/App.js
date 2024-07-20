dd29fad114fc857d6ee683454460d6cf2cf71520
shreya gore <118609660+shreya3222@users.noreply.github.com>
2024-07-20 14:46:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AllProducts from './pages/AllProducts';
+import ProductPage from './pages/ProductPage';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AllProducts />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
